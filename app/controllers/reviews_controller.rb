bcab537f60dbc4168df90b630a04beec0b3b61b7
Rachel R <rrrachelrath@gmail.com>
2019-07-11 14:00:11 -0400

    updated create/delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,7 +15,8 @@ class ReviewsController < ApplicationController
   end
 
   def update
-    review = Review.create(review_params)
+    review = Review.find_by(id: params[:id])
+    review.update(review_params)
     render json: review
   end
 
@@ -28,6 +29,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:familiar_id, :comment)
+    params.require(:review).permit(:familiar_id, :comment, :likes)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_215453) do
+ActiveRecord::Schema.define(version: 2019_07_11_133935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_215453) do
     t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes"
     t.index ["familiar_id"], name: "index_reviews_on_familiar_id"
   end
 
